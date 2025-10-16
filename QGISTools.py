038940eb39858ec43950b03f2b7465b449c34a6c
DHL <david.hernandez@uclm.es>
2025-10-16 18:01:35 +0200

    .

--- QGISTools.py
+++ QGISTools.py
@@ -4,7 +4,7 @@
 import os, sys
 from osgeo import gdal, osr, ogr
 
-current_path = os.path.dirname(__file__)
+current_path = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(current_path, '..'))
 
 from qgis.core import (QgsApplication, QgsDataSourceUri, QgsProject,
